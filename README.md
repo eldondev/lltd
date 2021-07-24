038340f89700ab84bfc992c249ad3b6b2c0ebe0e
Eldon Stegall <eldon@eldondev.com>
2021-07-24 03:39:52 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# A Simplified Logging Strategy
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"log"
+  "os"
+
+//	"github.com/itchyny/gojq"
+)
+
+func main() {
+ log.Printf("Starting")
+ fmt.Printf(os.Args[1])
+}
